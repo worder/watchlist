6483f8e5c9121dfbef38a0c2175b2f102b7f9cce
worder <worder@vda-team.com>
2021-08-18 21:53:50 +0700

    add phpunit resources

---
+++ test/config/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require "./app/vendor/autoload.php";
---
+++ test/src/Test/Unit/Util/DateTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Unit\Util;
+
+use PHPUnit\Framework\TestCase;
+use Wl\Utils\Date\Date;
+
+class DateTest extends TestCase
+{
+    public function testSomething(): void
+    {
+    }
+}
